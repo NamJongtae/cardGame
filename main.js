bd66f2ba5484d34e9e57b011e535afa7c370c7be
jongtae.nam <dark9737@gmail.com>
2023-03-30 16:03:43 +0900

    카드 게임

--- main.js
+++ main.js
@@ -1,105 +1,114 @@
-const container = document.querySelector(".container");
-const cardFront = document.querySelectorAll(".cardFront");
-const cardBack = document.querySelectorAll(".card_back");
-const timer = document.querySelector(".timer-box span");
-const startBtn = document.querySelector(".start-btn");
-const modal = document.querySelector(".modal");
-const stopBtn = document.querySelector(".stop-btn");
-const resetBtn = document.querySelector(".reset-btn");
-const audioBtn = document.querySelector(".audioBtn")
-const totalCard = 16;
-let cardArray = [];
-let completedCard = [];
-let randomArray = [];
-let randomArray2 = [];
+const $container = document.querySelector(".container");
+const $cardFront = document.querySelectorAll(".cardFront");
+const $cardBack = document.querySelectorAll(".card_back");
+const $timer = document.querySelector(".timer-box span");
+const $startBtn = document.querySelector(".start-btn");
+const $modal = document.querySelector(".modal-wrapper.gamestart");
+const $stopBtn = document.querySelector(".stop-btn");
+const $resetBtn = document.querySelector(".reset-btn");
+const $audioBtn = document.querySelector(".audioBtn");
+const $recordModal = document.querySelector(".modal-wrapper.record");
+const $timeRecord = document.querySelector(".time-record");
+const $recordBtn = document.querySelector(".record-btn");
+const $recordCancelBtn = document.querySelector(".cancel-btn");
+/* 카드 행 열 지정 곱 => 짝수만 가능 홀수 시 짝이 안맞음 너무 큰 수를 지정하면 화면에서 벗어남 */ 
+const level = {"lv1":{row:"4",col:"3",}, "lv2":{row:"5",col:"4"},"lv3":{row:'6',col:"5"}};
+const row = level.lv1.row;
+const col = level.lv1.col;
+const totalCard = row * col;
+const animal = ['🐒','🦍','🐈','🐇','🐎','🦌','🦏','🐄','🦔','🐖','🐑','🐪','🦘','🐘','🐁','🦥'];
+const cardArray = [];
+const completedCard = [];
+const randomArray = [];
+const randomArray2 = [];
 const soundArray = [];
 const soundArray2 = [];
 const soundArray3 = [];
+
 const bgm = new Audio();
 bgm.src = "../audio/game_bgm.mp3";
 bgm.volume = 0.3;
 bgm.loop = true;
-
+bgm.muted = true;
 let isStop = false;
 let checked = false;
 let startTime = 0;
 let totalTime = 0;
 let isMute = false;
-let timeInterval = setInterval(() => {
-  inner.innerHTML = Math.floor(
-    (new Date().getTime() - startTime.getTime()) / 1000
-  );
-}, 1000);
-clearInterval(timeInterval);
+let timeInterval;
 
-startBtn.addEventListener("click", () => {
+/* 버튼 클릭 이벤트 함수 추가 */
+$startBtn.addEventListener("click", () => {
+  $modal.classList.toggle("active");
   startGame();
 });
-audioBtn.addEventListener("click", () => {
-  isMute = !isMute;
-  bgm.muted = isMute;
-  audioBtn.style.backgroundImage = isMute ? 'url("../img/muteBtn.png")' : 'url("../img/audioBtn.png")';
-})
-resetBtn.addEventListener("click", resetGame);
+$audioBtn.addEventListener("click", () => {
+  $audioBtn.classList.toggle("mute");
+  bgm.muted = $audioBtn.classList.contains("mute") ? true : false;
+});
+$resetBtn.addEventListener("click", resetGame);
+$recordCancelBtn.addEventListener("click",()=>{
+  $recordModal.classList.toggle("active");
+});
+
 function startGame() {
-  modal.style.display = "none";
+  $container.style.gridTemplateColumns = `repeat(${row}, 150px)`;
+  $container.style.gridTemplateRows = `repeat(${col}, 120px)`;
   random(randomArray);
   random(randomArray2);
   soundSetting(soundArray, "../audio/card_effect.mp3");
   soundSetting(soundArray2, "../audio/card_effect2.mp3");
   soundSetting(soundArray3, "../audio/card_effect3.wav");
-  container.style.pointerEvents = "none";
+  $container.style.pointerEvents = "none";
   cardSetting();
   bgm.play();
   setTimeout(() => {
     let card = document.querySelectorAll(".card");
-    for (var i = 0; i < card.length; i++) {
+    for (let i = 0; i < card.length; i++) {
       card[i].classList.toggle("flipped"); // 카드 뒤집기
     }
-    container.style.pointerEvents = "auto"; // 게임시작 클릭 제한 해제
+    $container.style.pointerEvents = "auto"; // 게임시작 클릭 제한 해제
     startTime = new Date().getTime();
     timeInterval = setInterval(() => {
       totalTime = Math.floor(
         (new Date().getTime() - startTime) / 1000
       );
-      timer.innerHTML = totalTime;
+      $timer.innerHTML = totalTime;
     }, 1000);
   }, 800);
 }
 
 /*-------------------------------------카드 설정 관련 함수-------------------------------------*/
 function cardSetting() {
-  for (i = 0; i < totalCard; i++) {
-    const card = document.createElement("div");
-    const cardInner = document.createElement("div");
-    const cardFront = document.createElement("div");
-    const cardBack = document.createElement("div");
+  for (let i = 0; i < totalCard; i++) {
+    const $card = document.createElement("div");
+    const $cardInner = document.createElement("div");
+    const $cardFront = document.createElement("div");
+    const $cardBack = document.createElement("div");
 
-    card.setAttribute("class", "card");
-    card.addEventListener("click", click_card);
-    cardInner.setAttribute("class", "card-inner");
-    cardFront.setAttribute("class", "card-front");
-    cardBack.setAttribute("class", "card-back");
-    container.append(card);
-    card.append(cardInner);
-    cardInner.append(cardFront);
-    cardInner.append(cardBack);
+    $card.setAttribute("class", "card");
+    $card.addEventListener("click", click_card);
+    $cardInner.setAttribute("class", "card-inner");
+    $cardFront.setAttribute("class", "card-front");
+    $cardBack.setAttribute("class", "card-back");
+    $container.append($card);
+    $card.append($cardInner);
+    $cardInner.append($cardFront);
+    $cardInner.append($cardBack);
 
-    if (i < 8) {
-      cardFront.style.backgroundImage =
-        "url(./img/card" + randomArray[i] + ".png)";
-      card.setAttribute("name", randomArray[i]);
+    if (i < totalCard/2) {
+      $cardFront.innerHTML = animal[randomArray[i]];
+      $card.setAttribute("name", randomArray[i]);
     } else {
-      cardFront.style.backgroundImage =
-        "url(./img/card" + randomArray2[i % 8] + ".png)";
-      card.setAttribute("name", randomArray2[i % 8]);
+      $cardFront.innerHTML = animal[randomArray2[i % (totalCard/2)]];
+      $card.setAttribute("name", randomArray2[i % (totalCard/2)]);
     }
   }
 }
 
 function random(array) {
-  for (let i = 0; i < 8; i++) {
-    let randomNnum = Math.floor(Math.random() * 8 + 1);
+  for (let i = 0; i < totalCard/2; i++) {
+    let randomNnum = Math.floor(Math.random() * totalCard/2 + 1);
     if (array.indexOf(randomNnum) == -1) {
       array.push(randomNnum);
     } else {
@@ -108,6 +117,7 @@ function random(array) {
   }
 }
 
+// 카드 클릭 시 일어나는 함수 
 function click_card() {
   if (checked === false) {
     playSound(soundArray);
@@ -122,20 +132,21 @@ function click_card() {
     ) {
       // 카드 일치시
       setTimeout(() => playSound(soundArray3), 300);
-      console.log("카드맞춤!");
       completedCard.push(cardArray[0]);
       completedCard.push(cardArray[1]);
       cardArray[0].style.pointerEvents = "none";
       cardArray[1].style.pointerEvents = "none";
-      cardArray = [];
-
+      cardArray.splice(0);
+      
       checked = false;
-      if (completedCard.length === 16) {
+      if (completedCard.length === totalCard) {
         clearInterval(timeInterval);
-        setTimeout(
-          `alert('축하합니다. 게임 클리어~ 총 걸린 시간은 ${totalTime}초')`,
-          500
-        );
+        bgm.pause();
+       
+        setTimeout(()=> {
+         $timeRecord.innerHTML = totalTime;
+          $recordModal.classList.toggle("active");
+        },500);
       }
       return;
     }
@@ -148,7 +159,7 @@ function click_card() {
       cardArray[0].classList.toggle("flipped");
       cardArray[1].classList.toggle("flipped");
       cardArray[0].style.pointerEvents = "auto";
-      cardArray = [];
+      cardArray.splice(0);
       checked = false;
     }, 1000);
   }
@@ -178,7 +189,7 @@ function soundSetting(soundArray, soundSrc) {
 function playSound(sound) {
   for (let i = 0; i < sound.length; i++) {
     if (sound[i].paused) {
-      sound[i].muted = isMute;
+      sound[i].muted = $audioBtn.classList.contains("mute") ? true : false;
       // 재생중이 아닌 Audio객체를 찾아서
       sound[i].play(); // 1회만 재생하고
       break; // 반복문을 나간다.
@@ -187,18 +198,22 @@ function playSound(sound) {
 }
 /*----------------------------------// Audio 관련 함수------------------------------------ */
 
+// 게임 리셋 함수
 function resetGame() {
+  $resetBtn.style.pointerEvents = 'none';
   bgm.pause();
   bgm.load();
-  cardArray = [];
-  completedCard = [];
-  randomArray = [];
-  randomArray2 = [];
+  cardArray.splice(0);
+  completedCard.splice(0);
+  randomArray.splice(0);
+  randomArray2.splice(0);
   checked = false;
   clearInterval(timeInterval);
-  timer.innerHTML = 0;
-  while(container.hasChildNodes()){
-    container.firstChild.remove();
+  $timer.innerHTML = 0;
+  while($container.hasChildNodes()){
+    $container.firstChild.remove();
   }
   startGame();
+  setTimeout(()=>{  $resetBtn.style.pointerEvents = 'auto';}, 1200)
+
 }
